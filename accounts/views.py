0e8ffe9b098a0960c79ac5951901eb5f4dd71402
Arunesh kumar <aruneshkumar324@gmail.com>
2022-03-16 12:28:03 +0530

    10.3 : Register new user account

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 
 def login(request):
@@ -8,8 +9,32 @@ def login(request):
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'This is ERROR message.')
-        return redirect('register')
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username already exist')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'Email already exists')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(first_name=firstname, last_name=lastname, username=username, email=email, password=password)
+                    auth.login(request, user)
+                    messages.success(request, 'You are logged in.')
+                    return redirect('dashboard')
+                    user.save()
+                    messages.success(request, 'Register successfully')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Password not much with confirm password')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
